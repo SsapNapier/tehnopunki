09b5ea132173bf082e3783b194de0881befe2880
Ssap Napier <bagz984202489@gmail.com>
2020-05-31 01:09:13 +0300

    header_menu12

--- pages/blog (copy 1)/_postId/index.vue
+++ pages/blog (copy 1)/_postId/index.vue
@@ -52,6 +52,7 @@ export default {
  asyncData(context) {
         return context.app.$storyapi.get("cdn/stories", {
           version: process.env.NODE_ENV == "production" ? "published" : "draft",
+          starts_with: "blog/"
         }).then(res => {
           return {
             posts: res.data.stories.map(bp => {
